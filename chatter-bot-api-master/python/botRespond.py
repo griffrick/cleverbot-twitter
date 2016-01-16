c013457855e496d296e4119c7e138fb91b548360
azmabdul <abdulkadirazam@gmail.com>
2016-01-15 23:38:34 -0500

    added some stuff

--- chatter-bot-api-master/python/botRespond.py
+++ chatter-bot-api-master/python/botRespond.py
@@ -1,12 +1,34 @@
 from chatterbotapi import ChatterBotFactory, ChatterBotType
+import requests
+import twitter
+def ask(message):
+	s = bot1session.think(message)
+	return s
+def run_rampant(api):
+	
+	results= api.GetSearch(term="trump")
+	for tweet in results:
+		twitter_text = unicode(tweet.text).encode("utf8")
+		screen_name = tweet.user.screen_name
+		print twitter_text
+		reply= ask(twitter_text)
+		print "reply: "+reply
+		full_reply= "@"+screen_name+" "+reply
+		if(len(full_reply)<=140):
+			api.PostUpdate("@"+screen_name+" "+reply)
 
 factory = ChatterBotFactory()
 
 bot1 = factory.create(ChatterBotType.CLEVERBOT)
 bot1session = bot1.create_session()
+api = twitter.Api(consumer_key='',
+                      consumer_secret='',
+                      access_token_key='',
+                      access_token_secret='')
+print api.VerifyCredentials()
 
-def ask(message):
-	s = bot1session.think(message);
-	send(s)
+while(True):
+	run_rampant(api)
+	sleep(30)
 
-def send(message):
+# def send(message):
